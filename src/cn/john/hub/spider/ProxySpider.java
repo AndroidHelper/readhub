22e92c595e40b96c0f7f926854c07e8d7ea56f51
Johnlw <johnlw2010@gmail.com>
2017-06-20 21:56:49 +0800

    commit

--- src/cn/john/hub/spider/ProxySpider.java
+++ src/cn/john/hub/spider/ProxySpider.java
@@ -25,6 +25,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.LinkedBlockingQueue;
 
 import javax.annotation.PostConstruct;
 
@@ -34,6 +35,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import cn.john.hub.domain.Proxy;
@@ -41,76 +43,88 @@ import cn.john.hub.util.Consts;
 import cn.john.hub.util.HttpClient;
 
 /**
-
+ * 
  * @ClassName: ProxySpider
-
+ * 
  * @Description: TODO
-
+ * 
  * @author: John
-
+ * 
  * @date: 2017年6月20日 下午3:03:19
-
-
+ * 
+ * 
  */
 @Component
 public class ProxySpider {
-	
-	public ProxySpider(){
-		log.info("Constructing...");
-		proxyList = new ArrayList<Proxy>();
-	}
-	
-	private List<Proxy> proxyList;
+
+	public volatile LinkedBlockingQueue<Proxy> proxyQueue;
 	private HttpClient httpClient;
 	private final static Logger log = LogManager.getLogger("logger");
+	@Autowired
+	private TechWebSpider twSpider;
+	public ProxySpider() {
+		log.info("Constructing...");
+		proxyQueue = new LinkedBlockingQueue<Proxy>();
+	}
 	
 	@PostConstruct
-	public void init(){
+	public void init() {
 		log.info("Start fetching proxy...");
-		if(proxyList.size()==0){
-			httpClient = new HttpClient("220.160.10.95",808);
+		if (proxyQueue.size() == 0) {
+			httpClient = new HttpClient("1.85.121.116", 8118);
 			String html = httpClient.getData(Consts.PROXY_SITE);
 			parseProxyHtml(html);
 		}
+		log.info("Starting thread...");
+		Thread t = new Thread(twSpider);
+		t.start();
+		log.info("Thread started!");
 	}
-	
-	private void parseProxyHtml(String html){
+
+	private void parseProxyHtml(String html) {
 		log.info("Start parsing...");
 		Document doc = Jsoup.parse(html);
 		Element news = doc.getElementById("ip_list");
 		Elements trs = news.getElementsByTag("tr");
-		List<HashMap<Integer,Object>> list = new ArrayList<HashMap<Integer,Object>>();
-		for(Element e:trs){
+		List<HashMap<Integer, Object>> list = new ArrayList<HashMap<Integer, Object>>();
+		for (Element e : trs) {
 			Elements tds = e.getElementsByTag("td");
 			int i = 0;
-			HashMap<Integer,Object> map = new HashMap<Integer,Object>();
-			for(Element e1:tds){
+			HashMap<Integer, Object> map = new HashMap<Integer, Object>();
+			for (Element e1 : tds) {
 				i++;
-				if(i==2||i==3||i==5||i==6){
+				if (i == 2 || i == 3 || i == 5 || i == 6) {
 					map.put(i, e1.html());
 				}
-				if(i==7||i==8){
+				if (i == 7 || i == 8) {
 					String s = e1.child(0).attr("title").trim();
-					String s1 = s.substring(0, s.length()-1);
+					String s1 = s.substring(0, s.length() - 1);
 					map.put(i, s1);
 				}
 			}
-			if(i>1){
-				list.add(map);				
+			if (i > 1) {
+				list.add(map);
 			}
 		}
+		
 		Iterator<HashMap<Integer, Object>> it = list.iterator();
-		while(it.hasNext()){
-			HashMap<Integer,Object> map = it.next();
+		
+		while (it.hasNext()) {
+			HashMap<Integer, Object> map = it.next();
 			Proxy proxy = new Proxy();
 			proxy.setIpAddr((String) map.get(2));
 			proxy.setPort((String) map.get(3));
 			proxy.setAnonymity((String) map.get(5));
 			proxy.setType((String) map.get(6));
 			proxy.setSpeed((String) map.get(7));
-			proxy.setConnectTime( (String) map.get(8));
-			proxyList.add(proxy);
+			proxy.setConnectTime((String) map.get(8));
+			try {
+				proxyQueue.put(proxy);
+			} catch (InterruptedException e1) {
+				log.error(e1.getMessage());
+			}
 		}
-		log.info(proxyList);
+		log.info("Proxy saved!Size is "+proxyQueue.size());
 	}
+
 }
